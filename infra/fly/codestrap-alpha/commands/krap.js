e7bd176d9f2b72c16c820ca794d185301b46bf4c
doriansmiley <doriansmiley@siliconpublishing.com>
2022-11-13 08:41:42 -0800

    add new bot and deffered responses

--- infra/fly/codestrap-alpha/commands/krap.js
+++ infra/fly/codestrap-alpha/commands/krap.js
@@ -9,12 +9,12 @@ const openai = new OpenAIApi(configuration);
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bash-help')
-        .addStringOption(option => option.setName('question').setDescription('How do I print the contents of a directory?').setRequired(true))
-        .setDescription('Helps you learn bash programming with Open AI GPT 3'),
+        .setName('karpism')
+        .addStringOption(option => option.setName('quote').setDescription('How do I print the contents of a directory?').setRequired(true))
+        .setDescription('Translates Alex Karp into language a five-year-old can understand.'),
     async execute(interaction) {
-        let prompt = '# I need help with bash programming';
-        const question = interaction.options.getString('question');
+        let prompt = 'I need help understanding what Alex Karp, the CEO of Palantir, is saying. He is an expert in german philosophy and often quotes Hegel. Can you explain what he said below as if I was a 5-year-old:';
+        const question = interaction.options.getString('quote');
         debug(`question: ${question}`);
         debug(`interaction.user.bot: ${interaction.user.bot}`);
         if (interaction.user.bot) return;
