75e71a8c97e6c3abe26349381941c7f638998773
doriansmiley <doriansmiley@siliconpublishing.com>
2024-11-17 22:38:54 -0800

    fix linter error

--- codestrap-technologies/src/app/components/landing-page.tsx
+++ codestrap-technologies/src/app/components/landing-page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
-import { CircleDollarSign, Users, Brain, Rocket, Search, Menu, Moon, Sun, ChevronRight } from "lucide-react";
+import { CircleDollarSign, Users, Brain, Rocket, Menu, Moon, Sun, ChevronRight } from "lucide-react";
 import { useState, useEffect } from "react";
 
 import { Button } from "@codestrap/app/components/ui/button";
